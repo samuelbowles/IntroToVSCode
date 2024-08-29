2f6ee237ee5caf5bc0f789f102141512f2a52ec6
Samuel Bowles <samuel_bowles1@baylor.edu>
2024-08-29 16:43:24 -0500

    initial commit

---
+++ python1.py
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c 
+# y is not on our list because the debugger stops prior to execution, not after execution
+
+
+message = "Hello World!"
+
+print(message)
